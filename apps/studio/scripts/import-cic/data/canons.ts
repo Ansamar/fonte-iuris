e29898e1b11164e4b5002c86e247442936296ef4
Ansamar <124778721+Ansamar@users.noreply.github.com>
2026-08-29 10:01:00 +0200

    feat: include CIC canons 403-411

--- apps/studio/scripts/import-cic/data/canons.ts
+++ apps/studio/scripts/import-cic/data/canons.ts
@@ -1,9 +1,11 @@
 import {sampleCanons} from './canons.sample'
 import {canons375to380} from './canons.375-380'
 import {canons381to402} from './canons.381-402'
+import {canons403to411} from './canons.403-411'
 
 export const allCanons = [
   ...sampleCanons,
   ...canons375to380,
   ...canons381to402,
+  ...canons403to411,
 ]
