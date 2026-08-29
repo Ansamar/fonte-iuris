c50ca4b485a3a01bbd50010276535d043f426bcc
Ansamar <124778721+Ansamar@users.noreply.github.com>
2026-08-29 09:40:41 +0200

    feat: include CIC canons 381-402

--- apps/studio/scripts/import-cic/data/canons.ts
+++ apps/studio/scripts/import-cic/data/canons.ts
@@ -1,4 +1,9 @@
 import {sampleCanons} from './canons.sample'
 import {canons375to380} from './canons.375-380'
+import {canons381to402} from './canons.381-402'
 
-export const allCanons = [...sampleCanons, ...canons375to380]
+export const allCanons = [
+  ...sampleCanons,
+  ...canons375to380,
+  ...canons381to402,
+]
