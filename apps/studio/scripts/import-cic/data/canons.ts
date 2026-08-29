3db9ce24786349bea2631bc3fcd47acb35eca2ed
Ansamar <124778721+Ansamar@users.noreply.github.com>
2026-08-29 16:21:46 +0200

    feat: register CIC canons 503-510

--- apps/studio/scripts/import-cic/data/canons.ts
+++ apps/studio/scripts/import-cic/data/canons.ts
@@ -7,6 +7,7 @@ import {canons431to459} from './canons.431-459'
 import {canons460to468} from './canons.460-468'
 import {canons469to494} from './canons.469-494'
 import {canons495to502} from './canons.495-502'
+import {canons503to510} from './canons.503-510'
 
 export const allCanons = [
   ...sampleCanons,
@@ -18,4 +19,5 @@ export const allCanons = [
   ...canons460to468,
   ...canons469to494,
   ...canons495to502,
+  ...canons503to510,
 ]
