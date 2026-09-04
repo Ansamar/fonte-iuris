853b55de55766605139e73f6391f4db8cc3088d4
Ansamar <124778721+Ansamar@users.noreply.github.com>
2026-09-04 12:44:00 +0200

    feat: add Materie corpus page

---
+++ apps/web/src/app/materie/page.tsx
@@ -0,0 +1,22 @@
+import LegalCorpusShell from "../LegalCorpusShell";
+import MaterieBrowser from "./MaterieBrowser";
+
+const PROJECT_ID="2rq93txn";
+const DATASET="production";
+const API_VERSION="2026-03-25";
+
+type Concept={_id:string;label:string;definition:string;synonyms?:string[];broaderConcept?:{_id:string;label:string}|null;relatedCanons?:Array<{_id:string;number:number;editorialTitle?:string}>};
+
+async function loadConcepts():Promise<Concept[]>{
+  const query=`*[_type=="legalConcept"]|order(label asc){_id,label,definition,synonyms,"broaderConcept":broaderConcept->{_id,label},"relatedCanons":relatedCanons[]->{_id,number,editorialTitle}}`;
+  const url=new URL(`https://${PROJECT_ID}.api.sanity.io/v${API_VERSION}/data/query/${DATASET}`);
+  url.searchParams.set("query",query);
+  const res=await fetch(url,{cache:"no-store"});
+  if(!res.ok)throw new Error("Corpus materie non disponibile");
+  return (await res.json()).result??[];
+}
+
+export default async function MateriePage(){
+  const concepts=await loadConcepts();
+  return <LegalCorpusShell section="Navigazione tematica" activeSection="materie"><MaterieBrowser concepts={concepts}/></LegalCorpusShell>;
+}
